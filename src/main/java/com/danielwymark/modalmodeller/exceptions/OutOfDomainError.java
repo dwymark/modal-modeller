70c8381285fd06db49c4ac5a85d65551c0ae6858
Daniel Wymark <d.d.wymark@gmail.com>
2021-12-23 22:05:46 -0800

    Make OutOfDomainError a runtime error

--- src/main/java/com/danielwymark/modalmodeller/exceptions/OutOfDomainError.java
+++ src/main/java/com/danielwymark/modalmodeller/exceptions/OutOfDomainError.java
@@ -1,6 +1,6 @@
 package com.danielwymark.modalmodeller.exceptions;
 
-public class OutOfDomainError extends Exception {
+public class OutOfDomainError extends RuntimeException {
     public OutOfDomainError(String errorMessage) {
         super(errorMessage);
     }
