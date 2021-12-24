bbd861b0b1b8cc92248f4529fdf565574d0eb3d4
Daniel Wymark <d.d.wymark@gmail.com>
2021-12-23 22:06:16 -0800

    Implement Graphviz visualization for models

---
+++ src/main/java/com/danielwymark/modalmodeller/app/ShellApp.java
@@ -0,0 +1,33 @@
+package com.danielwymark.modalmodeller.app;
+
+import com.danielwymark.modalmodeller.model.Model;
+import com.danielwymark.modalmodeller.model.ModelBuilder;
+import guru.nidi.graphviz.engine.Format;
+import guru.nidi.graphviz.engine.Graphviz;
+
+import java.io.File;
+import java.io.IOException;
+
+public class ShellApp {
+    public static void main(String[] args) {
+        final int NUM_WORLDS = 4;
+        var modelBuilder = new ModelBuilder(NUM_WORLDS);
+        modelBuilder.addRelation(0, 0);
+        modelBuilder.addRelation(0, 1);
+        modelBuilder.addRelation(0, 2);
+        modelBuilder.addRelation(1, 2);
+        modelBuilder.addRelation(3, 1);
+        modelBuilder.addTruth(0, "p");
+        modelBuilder.addTruth(0, "q");
+        modelBuilder.addTruth(1, "p");
+        modelBuilder.addTruth(3, "q");
+
+        Model model = modelBuilder.build();
+        try {
+            Graphviz.fromGraph(model.generateGraph())
+                    .height(600).render(Format.PNG).toFile(new File("model.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/java/com/danielwymark/modalmodeller/model/Model.java
+++ src/main/java/com/danielwymark/modalmodeller/model/Model.java
@@ -2,8 +2,21 @@ package com.danielwymark.modalmodeller.model;
 
 import com.danielwymark.modalmodeller.exceptions.OutOfDomainError;
 import com.danielwymark.modalmodeller.syntax.SingularFormula;
+import guru.nidi.graphviz.attribute.Color;
+import guru.nidi.graphviz.attribute.Font;
+import guru.nidi.graphviz.attribute.Rank;
+import guru.nidi.graphviz.attribute.Style;
+import guru.nidi.graphviz.model.Graph;
+import guru.nidi.graphviz.model.Node;
 
 import java.util.*;
+
+import static guru.nidi.graphviz.attribute.Attributes.attr;
+import static guru.nidi.graphviz.attribute.Rank.RankDir.LEFT_TO_RIGHT;
+import static guru.nidi.graphviz.attribute.Rank.RankDir.TOP_TO_BOTTOM;
+import static guru.nidi.graphviz.model.Factory.graph;
+import static guru.nidi.graphviz.model.Factory.node;
+import static guru.nidi.graphviz.model.Link.to;
 
 /**
  * Represents a possible worlds model, which is defined by a set of worlds W,
@@ -27,6 +40,10 @@ public final class Model {
         this.valuationMap = valuationMap;
     }
 
+    /*::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+     * Accessors
+     */
+
     public List<World> getWorlds() {
         return Collections.unmodifiableList(worlds);
     }
@@ -36,12 +53,12 @@ public final class Model {
         return worldIndexMap.get(i);
     }
 
-    public Set<World> worldsAccessibleFrom(World world) throws OutOfDomainError {
+    public Set<World> worldsAccessibleFrom(World world) {
         assertInDomain(world.index);
         return Collections.unmodifiableSet(accessMap.get(world));
     }
 
-    public Set<SingularFormula> propositionsTrueAt(World world) throws OutOfDomainError {
+    public Set<SingularFormula> propositionsTrueAt(World world) {
         assertInDomain(world.index);
         return Collections.unmodifiableSet(valuationMap.get(world));
     }
@@ -58,10 +75,49 @@ public final class Model {
         return valuationMap;
     }
 
+    /*::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+     * Visualization
+     */
+
+    public Graph generateGraph() {
+        Graph graph = graph("Model " + hashCode()).directed()
+                .graphAttr().with(Rank.dir(TOP_TO_BOTTOM))
+                .nodeAttr().with(Font.name("sans-serif"))
+                .linkAttr().with("class", "link-class");
+
+        for (World world : worlds) {
+            var truePropositions = new ArrayList<String>();
+            for (SingularFormula formula : propositionsTrueAt(world)) {
+                truePropositions.add(formula.letter);
+            }
+
+            var label = Integer.toString(world.index);
+            if (truePropositions.size() > 0) {
+                label += "\n" + Arrays.toString(truePropositions.stream().sorted().toArray());
+            }
+
+            Node worldNode = node(world.toString()).with("label", label);
+            for (World neighbor : worldsAccessibleFrom(world)) {
+                worldNode = worldNode.link(neighbor.toString());
+            }
+            graph = graph.with(worldNode);
+        }
+
+        return graph;
+    }
+
+    /*::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+     * Helper functions
+     */
+
     private void assertInDomain(int i) throws OutOfDomainError {
         if (!worldIndexMap.containsKey(i))
             throw new OutOfDomainError("World " + i + " not in domain of model");
     }
+
+    /*::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+     * Standard overrides
+     */
 
     @Override
     public boolean equals(Object obj) {
@@ -86,6 +142,4 @@ public final class Model {
                 "valuationMap=" + valuationMap + ']';
     }
 
-
-    // TODO: implement a graphvis output function (in a separate class)
 }
