0ec66a617caf669f26579e5c7aae66b41ef60e74
Daniel Wymark <d.d.wymark@gmail.com>
2022-01-15 22:16:06 -0800

    Remove obsolete code

--- src/main/java/com/danielwymark/modalmodeller/relations/Partitioning.java
+++
@@ -1,9 +0,0 @@
-package com.danielwymark.modalmodeller.relations;
-
-import com.danielwymark.modalmodeller.model.World;
-
-import java.util.List;
-import java.util.Set;
-
-public record Partitioning(Set<World> universe, List<Set<World>> blocks) {
-}
